c944bc1b764a35efe10572303fb589a6b03282d7
Macbook pro retina <devarthurribeiro@gmail.com>
2020-01-16 06:36:35 -0300

    adiconado mapemanto de setores por subredes

---
+++ src/util/sections.js
@@ -0,0 +1,146 @@
+export default [
+  {
+    name: "INF",
+    init: 12,
+    end: 15,
+    vlan_id: 1301
+  },
+  {
+    name: "ETC",
+    init: 16,
+    end: 19,
+    vlan_id: 1302
+  },
+  {
+    name: "CVT",
+    init: 20,
+    end: 23,
+    vlan_id: 1303
+  },
+  {
+    name: "ESM",
+    init: 24,
+    end: 27,
+    vlan_id: 1304
+  },
+  {
+    name: "GRD",
+    init: 28,
+    end: 31,
+    vlan_id: 1305
+  },
+  {
+    name: "NAG",
+    init: 32,
+    end: 35,
+    vlan_id: 1306
+  },
+  {
+    name: "TDS",
+    init: 36,
+    end: 39,
+    vlan_id: 1307
+  },
+  {
+    name: "DIR",
+    init: 40,
+    end: 43,
+    vlan_id: 1308
+  },
+  {
+    name: "BIB",
+    init: 44,
+    end: 47,
+    vlan_id: 1309
+  },
+  {
+    name: "RSU",
+    init: 48,
+    end: 51,
+    vlan_id: 1310
+  },
+  {
+    name: "CAQ",
+    init: 52,
+    end: 55,
+    vlan_id: 1311
+  },
+  {
+    name: "EAQ",
+    init: 56,
+    end: 59,
+    vlan_id: 1312
+  },
+  {
+    name: "GTI",
+    init: 60,
+    end: 63,
+    vlan_id: 1313
+  },
+  {
+    name: "NUT",
+    init: 64,
+    end: 67,
+    vlan_id: 1314
+  },
+  {
+    name: "POS",
+    init: 68,
+    end: 71,
+    vlan_id: 1315
+  },
+  {
+    name: "ALM",
+    init: 72,
+    end: 75,
+    vlan_id: 1316
+  },
+  {
+    name: "ALF",
+    init: 76,
+    end: 79,
+    vlan_id: 1317
+  },
+  {
+    name: "CPE",
+    init: 80,
+    end: 83,
+    vlan_id: 1318
+  },
+  {
+    name: "CAG",
+    init: 84,
+    end: 87,
+    vlan_id: 1319
+  },
+  {
+    name: "EST",
+    init: 88,
+    end: 91,
+    vlan_id: 1320
+  },
+  {
+    name: "SOB",
+    init: 92,
+    end: 95,
+    vlan_id: 1321
+  },
+  {
+    name: "AGV",
+    init: 96,
+    end: 99,
+    vlan_id: 1322
+  },
+  {
+    name: "SDR",
+    init: 100,
+    end: 103,
+    vlan_id: 1323
+  },
+  {
+    name: "GSL",
+    init: 104,
+    end: 107,
+    vlan_id: 1324
+  },
+]
