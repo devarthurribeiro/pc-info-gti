5e3a65148b4db71589ba9573d545b5788bcbead2
Arthur Ribeiro <devarthurribeiro@gmail.com>
2020-02-01 02:19:40 -0300

    add section description

--- src/sections.js
+++ src/sections.js
@@ -1,146 +1,170 @@
 module.exports = [
   {
     name: "INF",
+    description: "INFORMÁTICA",
     init: 12,
     end: 15,
     vlan_id: 1301
   },
   {
     name: "ETC",
+    description: "ETEC",
     init: 16,
     end: 19,
     vlan_id: 1302
   },
   {
     name: "CVT",
+    description: "CVT",
     init: 20,
     end: 23,
     vlan_id: 1303
   },
   {
     name: "ESM",
+    description: "ENSINO MÉDIO",
     init: 24,
     end: 27,
     vlan_id: 1304
   },
   {
     name: "GRD",
+    description: "GRADUAÇÃO",
     init: 28,
     end: 31,
     vlan_id: 1305
   },
   {
     name: "NAG",
+    description: "NOVA AGRO",
     init: 32,
     end: 35,
     vlan_id: 1306
   },
   {
     name: "TDS",
+    description: "TADS",
     init: 36,
     end: 39,
     vlan_id: 1307
   },
   {
     name: "DIR",
+    description: "DIREÇÃO",
     init: 40,
     end: 43,
     vlan_id: 1308
   },
   {
     name: "BIB",
+    description: "BIBLIOTECA",
     init: 44,
     end: 47,
     vlan_id: 1309
   },
   {
     name: "RSU",
+    description: "RU",
     init: 48,
     end: 51,
     vlan_id: 1310
   },
   {
     name: "CAQ",
+    description: "COORDENAÇÃO AQUICULTURA",
     init: 52,
     end: 55,
     vlan_id: 1311
   },
   {
     name: "EAQ",
+    description: "ESTAÇÃO AQUICULTURA",
     init: 56,
     end: 59,
     vlan_id: 1312
   },
   {
     name: "GTI",
+    description: "GERENCIA DE TI",
     init: 60,
     end: 63,
     vlan_id: 1313
   },
   {
     name: "NUT",
+    description: "NUTRIÇÃO ANIMAL",
     init: 64,
     end: 67,
     vlan_id: 1314
   },
   {
     name: "POS",
+    description: "",
     init: 68,
     end: 71,
     vlan_id: 1315
   },
   {
     name: "ALM",
+    description: "ALOJAMENTO MASCULINO",
     init: 72,
     end: 75,
     vlan_id: 1316
   },
   {
     name: "ALF",
+    description: "ALOJAMENTO FEMININO",
     init: 76,
     end: 79,
     vlan_id: 1317
   },
   {
     name: "CPE",
+    description: "COPE",
     init: 80,
     end: 83,
     vlan_id: 1318
   },
   {
     name: "CAG",
+    description: "COORDENAÇÃO AGROPECUARIA",
     init: 84,
     end: 87,
     vlan_id: 1319
   },
   {
     name: "EST",
+    description: "ESTABULO",
     init: 88,
     end: 91,
     vlan_id: 1320
   },
   {
     name: "SOB",
+    description: "SOBRADO",
     init: 92,
     end: 95,
     vlan_id: 1321
   },
   {
     name: "AGV",
+    description: "VIDEO AGRO",
     init: 96,
     end: 99,
     vlan_id: 1322
   },
   {
     name: "SDR",
+    description: "SALA DO RÁDIO",
     init: 100,
     end: 103,
     vlan_id: 1323
   },
   {
     name: "GSL",
+    description: "GESOLO",
     init: 104,
     end: 107,
     vlan_id: 1324
-  },
-]
+  }
+];
